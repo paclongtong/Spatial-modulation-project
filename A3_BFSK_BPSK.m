59f22f383ec22ef3300001d527d854875ef2ef3c
Paclong <11412691+paclong@user.noreply.gitee.com>
2023-01-09 13:47:36 +0100

    updated Jan 9th, 2023

--- A3_BFSK_BPSK.m
+++ A3_BFSK_BPSK.m
@@ -1,9 +1,9 @@
-%** Combining 3 antennas, BFSK and BPSK**%
+%** Combining 4 antennas, BFSK and BPSK**%
 clear;
 format long;
 N = 8000;                                  %** Number of Transmitted symbols   **%
 M = 16;
-nbits = 4;                                 % Number of bits per symbol   %
+
 met = zeros(1,16);
                                            % BPSK Bit   BFSK Bit  %
 s1 = [1, 0];                               %  0           0         %
@@ -11,14 +11,88 @@ s2 = [0, 1];                               %  0           1         %
 s3 = [-1, 0];                              %  1           0         %  
 s4 = [0, -1];                              %  1           1         %
     
-for l=1:11
-    np = 1.0*(l-1);                        %** Eb/N0 in dB
-    a = (0.5/nbits)*10^(-np/10);           %** Noise variance in linear                  **%
+nbits = 3;                                 % Number of bits per symbol   %
+for l=1:17
+%     np = 1.0*(l-1);                        %** Eb/N0 in dB
+%     a = (0.5/nbits)*10^(-np/10);           %** Noise variance in linear                  **%
+    
+    np = 2.5*(l-1);                               %** E_b/N_0 in dB = 10*log10(snr)  **%
+    a = (0.5/4)*10^(-np/10);                      %** Variance of the thermal noise  N0/2 = 0.5(N0/Es) = 0.5(N0/Eb)/2 **%
+    es = 0;
+%     a=0;
+%     h = crand(2,1);                        %** Fading channel (assumed Rayleigh fading)  **%
+%     h = ones(2,1);                         %** Ideal channel  (AWGN only)                **%
+%     es = 0;
+    for k=1:1:N
+      x=round(1*rand(1,3));                %** Input bits (3 bits per symbol)            **%
+      h = crandn(2,1);
+      if (x(3)==0)
+          if (x(1) == 0 && x(2) == 0)   
+              rd1 = h(1)*s1 + sqrt(a)*crandn(1,2);  %** received signal point for 00              **%
+          elseif (x(1) == 0 && x(2) == 1)
+              rd1 = h(1)*s2 + sqrt(a)*crandn(1,2);  %** received signal point for 01              **%
+          elseif (x(1) == 1 && x(2) == 0)
+              rd1 = h(1)*s3 + sqrt(a)*crandn(1,2);  %** received signal point for 10              **%
+          else
+              rd1 = h(1)*s4 + sqrt(a)*crandn(1,2);  %** received signal point for 11              **%
+          end
+      else
+          if (x(1) == 0 && x(2) == 0)   
+              rd1 = h(2)*s1 + sqrt(a)*crandn(1,2);  %** received signal point for 00              **%
+          elseif (x(1) == 0 && x(2) == 1)
+              rd1 = h(2)*s2 + sqrt(a)*crandn(1,2);  %** received signal point for 01              **%
+          elseif (x(1) == 1 && x(2) == 0)
+              rd1 = h(2)*s3 + sqrt(a)*crandn(1,2);  %** received signal point for 10              **%
+          else
+              rd1 = h(2)*s4 + sqrt(a)*crandn(1,2);  %** received signal point for 11              **%
+          end
+      end
+    
+      %**  Metric calculations    **%
+      met2(1) = sum(abs(rd1 - h(1)*s1).^2);      %  0 0 0%
+      met2(2) = sum(abs(rd1 - h(2)*s1).^2);      %  0 0 1%
+      met2(3) = sum(abs(rd1 - h(1)*s2).^2);      %  0 1 0%
+      met2(4) = sum(abs(rd1 - h(2)*s2).^2);      %  0 1 1%
+      met2(5) = sum(abs(rd1 - h(1)*s3).^2);      %  1 0 0%
+      met2(6) = sum(abs(rd1 - h(2)*s3).^2);      %  1 0 1%
+      met2(7) = sum(abs(rd1 - h(1)*s4).^2);      %  1 1 0%
+      met2(8) = sum(abs(rd1 - h(2)*s4).^2);      %  1 1 1%
+      [med,nd] = min(met2);
+
+      out_sym = nd -1;                                               %** Output symbol decided by the decoder  **%
+%       disp(out_sym);
+      out_bit(2) = bitget(out_sym,2);                                %** Second bit  **%
+      out_bit(1) = bitget(out_sym,3);                                %** First bit **%
+      out_bit(3) = bitget(out_sym,1);
+      es = es + bitxor(x(1),out_bit(1)) + bitxor(x(2),out_bit(2)) + bitxor(x(3), out_bit(3));   %** Counting the number of wrong bits  **%
+    end
+    
+%     ber = zeros(1,1000000);
+%     snr = zeros(1,1000000);
+    ber2(l) = es/(3*N);                                                %** Calculating the average bit error probability  **%
+    snr(l) = np;
+    es = 0;
+%     srt = 10^(np/10);
+%      b_BFSK(l) = 0.5*erfc(sqrt(srt/2));
+%      b_BPSK(l) = 0.5*erfc(sqrt(srt));
+    
+    N = 200/ber2(l);                                                   %** Number of transmitted symbols (updated)  **%
+end
+
+N = 800;
+nbits = 4;                                 % Number of bits per symbol   %
+for l=1:17
+%     np = 1.0*(l-1);                        %** Eb/N0 in dB
+%     a = (0.5/nbits)*10^(-np/10);           %** Noise variance in linear                  **%
+        np = 2.5*(l-1); 
+     a = (0.5/nbits)*10^(-np/10); 
 %     a=0;
 %     h = crand(2,1);                        %** Fading channel (assumed Rayleigh fading)  **%
 %     h = ones(2,1);                         %** Ideal channel  (AWGN only)                **%
 %     h = crandn(4,1);
     es = 0;
+    
+    
     for k=1:1:N
       x=round(1*rand(1,4));                %** Input bits (3 bits per symbol)            **%
       h = crandn(4,1);
@@ -92,6 +166,8 @@ for l=1:11
       es = es + bitxor(x(1),out_bit(1)) + bitxor(x(2),out_bit(2)) + bitxor(x(3), out_bit(3) + bitxor(x(4), out_bit(4)));   %** Counting the number of wrong bits  **%
     end
     
+    
+    
 %     ber = zeros(1,1000000);
 %     snr = zeros(1,1000000);
     ber(l) = es/(4*N);                                                %** Calculating the average bit error probability  **%
@@ -104,9 +180,56 @@ for l=1:11
     N = 200/ber(l);                                                   %** Number of transmitted symbols (updated)  **%
 end
 
+N=800;
+out_bit = zeros(2);
+for l=1:17
+%     np = 1.0*(l-1);                        %** Eb/N0 in dB
+%     a = (0.5/nbits)*10^(-np/10);           %** Noise variance in linear                  **%
+            np = 2.5*(l-1); 
+     a = (0.5/2)*10^(-np/10); 
+%     h = crand(2,1);                        %** Fading channel (assumed Rayleigh fading)  **%
+%     h = ones(2,1);                         %** Ideal channel  (AWGN only)                **%
+    es = 0;
+    for k=1:1:N
+        h = crand(2,1);
+      x=round(1*rand(1,2));                %** Input bits (2 bits per symbol)            **%
+	  if (x(1) == 0 && x(2) == 0)   
+          rd1 = s1 + sqrt(a)*crandn(1,1);  %** received signal point for 00              **%
+      elseif (x(1) == 0 && x(2) == 1)
+          rd1 = s2 + sqrt(a)*crandn(1,1);  %** received signal point for 01              **%
+      elseif (x(1) == 1 && x(2) == 0)
+          rd1 = s3 + sqrt(a)*crandn(1,2);  %** received signal point for 10              **%
+      else
+          rd1 = s4 + sqrt(a)*crandn(1,2);  %** received signal point for 11              **%
+      end
+      %**  Metric calculations    **%
+      met1(1) = sum(abs(rd1 - h(1)*s1).^2);      %  0 0 %
+      met1(2) = sum(abs(rd1 - h(1)*s2).^2);      %  0 1 %
+      met1(3) = sum(abs(rd1 - h(1)*s3).^2);      %  1 0 %
+      met1(4) = sum(abs(rd1 - h(1)*s4).^2);      %  1 1 %
+      [med,nd] = min(met1);
+
+      out_sym = nd -1;                                               %** Output symbol decided by the decoder  **%
+      out_bit(2) = bitget(out_sym,1);                                %** First bit  **%
+      out_bit(1) = bitget(out_sym,2);                                %** Second bit **%
+      es = es + bitxor(x(1),out_bit(1)) + bitxor(x(2),out_bit(2));   %** Counting the number of wrong bits  **%
+	end
+
+    ber1(l) = es/(2*N);                                                %** Calculating the average bit error probability  **%
+    snr(l) = np;
+    es = 0;
+    srt = 10^(np/10);
+    b_BFSK(l) = 0.5*erfc(sqrt(srt/2));
+    b_BPSK(l) = 0.5*erfc(sqrt(srt));
+    
+    N = 200/ber1(l);                                                   %** Number of transmitted symbols (updated)  **%
+end
+
+
 % semilogy(snr,ber,'bo-',snr,b_BFSK)
-semilogy(snr,ber,'bo-', snr, b_BFSK, snr, b_BPSK)
+semilogy(snr,ber1,snr,ber2,snr,ber,'bo-', snr, b_BFSK, snr, b_BPSK)
+legend('BPSK and BFSK','2-antenna SM','4-antenna SM', 'BFSK','BPSK');
 grid
 xlabel('SNR, in dB')
 ylabel('BER')
-axis([0 12 1e-10 1])
+axis([0 25 1e-6 1])
